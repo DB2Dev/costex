01d34fb0f5179b92551eda7ae402c8701e468e0c
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-10-29 02:23:21 +0300

    add TODOs

--- app/utils/copy_to_db.py
+++ app/utils/copy_to_db.py
@@ -1,5 +1,12 @@
 from config import db
 
+# TODO: Fix the error below
+# psycopg2.errors.BadCopyFileFormat: extra data after last expected column
+# CONTEXT:  COPY employee, line 1: "Peter,N,Woods,2022-04-26,"1148 Samantha Park Apt. 624, Amberton, MI 15027""
+# TODO: Fix the error below
+# from app.config import db
+# ModuleNotFoundError: No module named 'app'
+
 def copy_csv_to_db():
     # Get the connection from db.py
     conn = db.get_connection()
