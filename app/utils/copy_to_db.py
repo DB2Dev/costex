98828076519bce976c5cf909c7b3a9f98ac41ca4
Omar Elshenhabi <amrmawaheb65@gmail.com>
2023-10-30 02:02:51 +0300

    add pre-commit

--- app/utils/copy_to_db.py
+++ app/utils/copy_to_db.py
@@ -1,8 +1,5 @@
 from app.config import db
 
-# TODO: Fix the error below
-# from app.config import db
-# ModuleNotFoundError: No module named 'app'
 
 def copy_csv_to_db():
     # Get the connection from db.py
@@ -10,19 +7,20 @@ def copy_csv_to_db():
     cur = conn.cursor()
 
     # Copy data from CSV to the Employee table
-    with open('data/employees.csv', 'r', encoding='utf-8') as f:
+    with open("data/employees.csv", "r", encoding="utf-8") as f:
         next(f)  # Skip the header
-        cur.copy_from(f, 'employee', sep=',')
+        cur.copy_from(f, "employee", sep=",")
 
     # Copy data from CSV to the Project table
-    with open('data/projects.csv', 'r', encoding='utf-8') as f:
+    with open("data/projects.csv", "r", encoding="utf-8") as f:
         next(f)  # Skip the header
-        cur.copy_from(f, 'project', sep=',')
+        cur.copy_from(f, "project", sep=",")
 
     # Commit and close
     conn.commit()
     cur.close()
     conn.close()
+
 
 if __name__ == "__main__":
     copy_csv_to_db()
