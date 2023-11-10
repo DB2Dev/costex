5714277a2a0e3619bfa29acad35289fc21a63bfb
Osama Hardan <90199470+ohardan2@users.noreply.github.com>
2023-11-10 22:29:48 +0300

    added cost functions in 'select.py' + craeted cardinality.py

--- app/estimators/select.py
+++ app/estimators/select.py
@@ -1,7 +1,7 @@
 import math
 
 
-def file_scan(numOfBlocks: int = 55) -> int:
+def file_scan(numOfBlocks: int) -> int:
     return numOfBlocks / 2
 
 
@@ -13,9 +13,21 @@ def binary_search_range(numOfBlocks: int, selectivity: int, bfr: int) -> int:
     return math.log2(numOfBlocks) + (selectivity / bfr) - 1
 
 
-# primary index
-# secondary index
-# clustered index
+def primary_index_equal(height: int) -> int:
+    return height + 1
 
-# range query
-# equality query
+
+def primary_index_range(height: int, numOfBlocks: int) -> int:
+    return height + (numOfBlocks / 2)
+
+
+def clustering_index(height: int, selectivity: int, bfr: int) -> int:
+    return height + (selectivity / bfr)
+
+
+def secondary_index_equal(height: int, selectivity: int) -> int:
+    return height + 1 + selectivity
+
+
+def secondary_index_range(height: int, first_level_blocks: int, numOfTuples) -> int:
+    return height + (first_level_blocks / 2) + (numOfTuples / 2)
