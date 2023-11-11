dfff2d9ef77b76f371bec705114394e307b1b400
Osama Hardan <90199470+ohardan2@users.noreply.github.com>
2023-11-12 00:25:17 +0300

    finish join functions + main when only 1 filter

--- app/controller/main.py
+++ app/controller/main.py
@@ -1,3 +1,5 @@
+import app.estimators.select as estimate
+from app.models.metadata.tables_info import TablesDetails
 from app.models.query import Query
 from app.models.enums.queryOperation import QueryOperation
 from app.models.enums.filterOperator import FilterOperator
@@ -6,8 +8,10 @@ from app.models.enums.queryType import QueryType
 from app.models.enums.tableType import TableType
 from app.models.enums.logicalOperator import LogicalOperator
 from app.models.condition import Condition
-from typing import Dict
+from typing import Dict, List, Tuple
 from pprint import pprint
+
+from app.utils.cardinality import cardinality
 from .tokeniser import tokenise
 
 
@@ -20,11 +24,90 @@ def pipeline(sql: Query | str) -> Dict[AlgoChoice, int]:
     if len(sql.cost) == 0:
         if sql.operation == QueryOperation.SELECT:
             if len(sql.filters) == 1:
-                # directly estimate the query cost
-                pass
+                for algo in sql.filters[0].possible_algorithms:
+                    if algo == AlgoChoice.FILE_SCAN:
+                        sql.cost[AlgoChoice.FILE_SCAN] = estimate.file_scan(
+                            TablesDetails.get_no_of_blocks(sql.table_name)
+                        )
+
+                    elif algo == AlgoChoice.BINARY_SEARCH:
+                        if sql.filters[0].condition_type == QueryType.EQUALITY:
+                            sql.cost[
+                                AlgoChoice.BINARY_SEARCH
+                            ] = estimate.binary_search_equal(
+                                TablesDetails.get_no_of_blocks(sql.table_name)
+                            )
+                        else:
+                            sql.cost[
+                                AlgoChoice.BINARY_SEARCH
+                            ] = estimate.binary_search_range(
+                                TablesDetails.get_no_of_blocks(sql.table_name),
+                                TablesDetails.get_no_of_records(sql.table_name),
+                                cardinality(
+                                    sql.filters[0].column,
+                                    sql.table_name,
+                                    QueryType.RANGE,
+                                ),
+                            )
+
+                    elif algo == AlgoChoice.PRIMARY_INDEX:
+                        if sql.filters[0].condition_type == QueryType.EQUALITY:
+                            sql.cost[
+                                AlgoChoice.PRIMARY_INDEX
+                            ] = estimate.primary_index_equal()
+                        else:
+                            sql.cost[
+                                AlgoChoice.PRIMARY_INDEX
+                            ] = estimate.primary_index_range(
+                                TablesDetails.get_no_of_blocks(sql.table_name),
+                            )
+
+                    elif algo == AlgoChoice.CLUSTERED_INDEX:
+                        sql.cost[
+                            AlgoChoice.CLUSTERED_INDEX
+                        ] = estimate.clustering_index(
+                            cardinality(
+                                sql.filters[0].column,
+                                sql.table_name,
+                                sql.filters[0].condition_type,
+                            ),
+                            TablesDetails.get_no_of_records(sql.table_name)
+                            / TablesDetails.get_no_of_blocks(sql.table_name),
+                        )
+
+                    elif algo == AlgoChoice.SECONDARY_INDEX:
+                        if sql.filters[0].condition_type == QueryType.EQUALITY:
+                            sql.cost[
+                                AlgoChoice.SECONDARY_INDEX
+                            ] = estimate.secondary_index_equal(
+                                cardinality(
+                                    sql.filters[0].column,
+                                    sql.table_name,
+                                    sql.filters[0].condition_type,
+                                ),
+                            )
+                        else:
+                            sql.cost[
+                                AlgoChoice.SECONDARY_INDEX
+                            ] = estimate.secondary_index_range(
+                                TablesDetails.get_no_of_records(sql.table_name)
+                            )
             else:
-                # 1. order the query conditions by cardinality
+                cardinalities: List[Tuple[int, float]] = []
+                for index, condition in enumerate(sql.filters):
+                    cardinalities.append(
+                        (
+                            index,
+                            cardinality(
+                                condition.column_name,
+                                sql.table_name,
+                                condition.condition_type,
+                            ),
+                        )
+                    )
+                cardinalities = sorted(cardinalities, key=lambda x: x[1])
                 # 2. estimate the query cost
+                sql.filters[cardinalities[0][0]]
                 pass
         elif sql.operation == QueryOperation.JOIN:
             # directly estimate the query cost
--- app/utils/cardinality.py
+++ app/utils/cardinality.py
@@ -10,8 +10,10 @@ def cardinality(attr: Column, table_name: TableType, query_type: QueryType) -> f
         return 1
     elif query_type == QueryType.EQUALITY and not attr.is_unique:
         for row in Distributions():
-            if row[0] == table_name.value:
+            if row[1] == attr.name:
                 no_of_distinct = row[3]
+            if no_of_distinct < 0:
+                no_of_distinct = abs(no_of_distinct) * TablesDetails.get_no_of_records(table_name)
         return TablesDetails.get_no_of_records(table_name) / no_of_distinct
     elif query_type == QueryType.RANGE:
         return TablesDetails.get_no_of_records(table_name) / 2
