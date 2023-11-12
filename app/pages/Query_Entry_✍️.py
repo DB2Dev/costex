b6d71874e3bffeaa8e1cc2af46ecfe28afe6e513
Aly Soliman <alysoliman05@gmail.com>
2023-11-12 15:05:46 +0300

    tweak: `Query_Entry` change to horizontal alignment

--- app/pages/Query_Entry_✍️.py
+++ app/pages/Query_Entry_✍️.py
@@ -31,11 +31,12 @@ def query_builder():
     if join_checkbox:
         # Dropdown for selecting table to join
         join_table = st.selectbox('Select Table to INNER JOIN', ['table1', 'table2', 'table3'])
-
+        
+        row1_columns = st.columns(3)
         # Condition for INNER JOIN
-        condition_col1 = st.selectbox(f'Select Column from {selected_table}', ['col1', 'col2', 'col3'])
-        condition_sign = st.selectbox('Equal Sign', ['='])
-        condition_col2 = st.selectbox(f'Select Column from {join_table}', ['col1', 'col2', 'col3'], key='join_condition_col2')
+        condition_col1 =  row1_columns[0].selectbox(f'Select Column from {selected_table}', ['col1', 'col2', 'col3'])
+        condition_sign =  row1_columns[1].selectbox('Equal Sign', ['='])
+        condition_col2 =  row1_columns[2].selectbox(f'Select Column from {join_table}', ['col1', 'col2', 'col3'], key='join_condition_col2')
 
         join_condition = f"INNER JOIN {join_table} ON {selected_table}.{condition_col1} {condition_sign} {join_table}.{condition_col2}"
     else:
@@ -44,20 +45,26 @@ def query_builder():
    
     # WHERE clause
     st.subheader('WHERE')
-    condition_col = st.selectbox(f'Select Column from {selected_table}', ['col1', 'col2', 'col3'], key='where_condition_col')
-    condition_sign = st.selectbox('Select Sign', ['=', '>', '<', '>=', '<='], key='where_condition_sign')
-    condition_value = st.text_input('Enter Value', '', key='where_condition_value')
+    row1_columns = st.columns(3)
+    condition_col = row1_columns[0].selectbox(f'Select Column from {selected_table}', ['col1', 'col2', 'col3'], key='where_condition_col')
+    condition_sign = row1_columns[1].selectbox('Select Sign', ['=', '>', '<', '>=', '<='], key='where_condition_sign')
+    condition_value = row1_columns[2].text_input('Enter Value', '', key='where_condition_value')
     where_clause = f"WHERE ({selected_table}.{condition_col} {condition_sign} '{condition_value}'"
 
-    # Additional conditions
+      # Additional conditions
     i = 1
     st.subheader('CONDITIONS')
+    
     while st.checkbox(f'EXTRA CONDITION', key=f'extra_condition_{i}'):
-        condition_col = st.selectbox(f'Select Column from {selected_table}', ['col1', 'col2', 'col3'], key=f'extra_condition_col_{i}')
-        condition_sign = st.selectbox('Select Sign', ['=', '>', '<', '>=', '<='], key=f'extra_condition_sign_{i}')
-        condition_value = st.text_input('Enter Value', '', key=f'extra_condition_value_{i}')
         
-        condition_logic = st.radio(f'Select Condition Logic for Condition {i}:', ['AND', 'OR'], key=f'condition_logic_{i}')
+        st.write(f'Condition {i}')
+        row1_columns = st.columns(3)
+        row1_columns[0].selectbox('Column', ['col1', 'col2', 'col3'], key=f'extra_condition_col_{i}')
+        row1_columns[1].selectbox('Sign', ['=', '>', '<', '>=', '<='], key=f'extra_condition_sign_{i}')
+        row1_columns[2].text_input('Value', '', key=f'extra_condition_value_{i}')
+        
+
+        condition_logic = st.radio(f'Logic', ['AND', 'OR'], key=f'condition_logic_{i}')
         
         if condition_logic == 'OR':
             where_clause += f") {condition_logic} ({selected_table}.{condition_col} {condition_sign} '{condition_value}'"
