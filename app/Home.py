89fac2582daf451467445622befcb28f73e5a7bf
Aly Soliman <alysoliman05@gmail.com>
2023-11-13 21:45:10 +0300

    feat: create `Query_Cost_Report`

--- app/Home.py
+++ app/Home.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from models.metadata.attrs_constraints import Constraints
+from models.metadata.distributions import Distributions
+from models.metadata.tables_info import TablesDetails
+from models.metadata.indexes import Indexes
 
 # from pathlib import Path
 
@@ -8,25 +12,31 @@ import numpy as np
 
 
 def main():
+     
+    constraint = Constraints()
+    distribution = Distributions()
+    table_details = TablesDetails()    
+    index = Indexes()
+    
+   
+
+    
+    
+     
     #     print(path)
     st.title("Meta Data")
     # Sample data
-    data1 = np.random.randn(5, 5)
-    data2 = np.random.randn(5, 5)
-    data3 = np.random.randn(5, 5)
-    data4 = np.random.randn(5, 5)
-
     # Create DataFrames
-    df1 = pd.DataFrame(data1, columns=[f"Column {i+1}" for i in range(5)])
-    df2 = pd.DataFrame(data2, columns=[f"Column {i+1}" for i in range(5)])
-    df3 = pd.DataFrame(data3, columns=[f"Column {i+1}" for i in range(5)])
-    df4 = pd.DataFrame(data4, columns=[f"Column {i+1}" for i in range(5)])
+    # df1 = pd.DataFrame(data1, columns=[f"Column {i+1}" for i in range(5)])
+    # df2 = pd.DataFrame(data2, columns=[f"Column {i+1}" for i in range(5)])
+    # df3 = pd.DataFrame(data3, columns=[f"Column {i+1}" for i in range(5)])
+    # df4 = pd.DataFrame(data4, columns=[f"Column {i+1}" for i in range(5)])
 
     # Display tables
-    st.table(df1)
-    st.table(df2)
-    st.table(df3)
-    st.table(df4)
+    # st.table(df1)
+    # st.table(df2)
+    # st.table(df3)
+    # st.table(df4)
 
 
 if __name__ == "__main__":
--- app/pages/Query_Cost_Report📊  .py
+++ app/pages/Query_Cost_Report📊  .py
@@ -1,24 +1,88 @@
+
+from streamlit_echarts import st_echarts
 import streamlit as st
 from utils.qstore import retrieve_object
+from models.enums.algoChoice import AlgoChoice
+import pandas as pd
+
+
 
 
 def query_report_page(dictionary):
     st.write("### Query:")
     query = retrieve_object("sqlstr.dat")
     st.code(f"{query}")
-    st.write("### Displaying Dictionary:")
+    
+    
+    algo_choices= {
+        AlgoChoice.NESTED_LOOP_JOIN: "Nested Loop Join",
+        AlgoChoice.SORT_MERGE_JOIN: "Sort Merge Join",
+        AlgoChoice.BINARY_SEARCH: "Binary Search",
+        AlgoChoice.FILE_SCAN: "File Scan",
+        AlgoChoice.PRIMARY_INDEX: "Primary Index",
+        AlgoChoice.SECONDARY_INDEX: "Secondary Index",
+        AlgoChoice.CLUSTERED_INDEX: "Clustered Index",
+        AlgoChoice.INDEXED_NESTED_LOOP_JOIN: "Indexed Nested Loop Join",
+    }
+    
+    data_keys = []
+    data_values = []
+    
+    dictionary = dict(sorted(dictionary.items(), key=lambda item: item[1]))
+    
+    new_dictionary = {}
+    
     for key, value in dictionary.items():
-        st.write(f"**{key}:** {value}")
+        new_dictionary[algo_choices[key]] = value
+        data_keys.append(algo_choices[key])
+        data_values.append(value)
+    
 
-
+    
+    min_value = min(data_values)
+    data_values.remove(min_value)
+    max_value = max(data_values)
+    data_values.remove(max_value)
+    
+    
+        
+    options = {
+        "xAxis": {
+            "type": "category",
+            "data": data_keys,
+        },
+        "yAxis": {"type": "value", "max": max_value  },
+        "series": [
+            {
+                # add data_values to the chart and make sure to style the min and max values to be different
+                "data":[{"value":min_value, "itemStyle": {"color": "#03a900"}} ,*data_values, {"value":max_value, "itemStyle": {"color": "#d91600"}}],
+                "type": "bar",
+                "barMinHeight": "10"
+            }
+        ]
+    }
+    st_echarts(
+        options=options,
+        height="600px",
+    )
+    
+    st.table(pd.DataFrame.from_dict(new_dictionary, orient='index', columns=['Cost']))
+    
+    
+    
+    
+    
 def main():
-    st.title("Streamlit Dictionary Display")
+    st.title("Query Cost Statistics 📊")
 
     # Example dictionary (replace this with your own)
     costs = retrieve_object().cost
 
     # Display the dictionary
     query_report_page(costs)
+   
+    
+    
 
 
 if __name__ == "__main__":
